8368af375869f065c243e8bf67a5754cdc1be8a9
BEJI <119804271+MED27BJ@users.noreply.github.com>
2023-05-14 07:32:39 +0100

    Movie Up checkpoint react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+// the app import movieadd an movieList components and the data of the movies
+import { useState } from 'react';
 import './App.css';
+import MovieAdd from './components/MovieAdd';
+import MovieList from './components/MovieList';
+
+import { movieData } from './MovieData';
 
 function App() {
+  // the declaration of the useState for the movie data
+  const [movies,setMovies]=useState(movieData)
+  // this function add a new movie to the data movie
+  const handleAdd =(newMovie)=>{
+    setMovies([...movies,newMovie])
+    }
+    // state for search by title and rate
+    const [searchTitle,setSearchTitle]=useState("");
+    const [searchRate,setSearchRate]=useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MovieAdd setSearchTitle={setSearchTitle} setSearchRate={setSearchRate} funcAdd={handleAdd}></MovieAdd>
+       </header>
+      <main>
+      
+<MovieList searchTitle={searchTitle} searchRate={searchRate} movies={movies}></MovieList>
+      </main>
+      <footer>
+      </footer>
     </div>
   );
 }
-
+// export of data
 export default App;
---
+++ src/components/MovieList.jsx
@@ -0,0 +1,23 @@
+// This component returns the search by title and rate by filter and display the movie card component with map method
+// import of the movie card
+import MovieCard from './MovieCard'
+
+// the main function that use three parameters as props the data, search by title and search by rate
+const MovieList = ({movies,searchTitle,searchRate}) => {
+ 
+  
+  return (
+    <div>
+    <div className='movieList'>
+
+    {movies
+      .filter((el)=>(el.title.toLocaleLowerCase().includes(searchTitle.toLocaleLowerCase())) && (el.rating >= searchRate))
+      .map((el,index)=>(
+      <MovieCard key={index} movie={el}></MovieCard>
+      
+  ))}
+    </div>
+    </div>
+)}
+// export of the component
+export default MovieList
